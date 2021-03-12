d37a5290e9a11e8fc3ccf93c69807919b2ae7f90
Daniel Chalk <dchalk@adaptavist.com>
2021-03-12 10:27:59 +0000

    fix(config): environment not being honoured
    The environment is now taken into account and does not rely solely on the config file

---
+++ pkg/config/config_test.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const username = "uname"
+const password = "passwd"
+
+func env() {
+	os.Setenv("BPR_BITBUCKET_USERNAME", username)
+	os.Setenv("BPR_BITBUCKET_PASSWORD", password)
+}
+
+func clear() {
+	os.Clearenv()
+}
+
+func TestConfigBitbucketEnv(t *testing.T) {
+	env()
+	config, err := LoadConfig(false)
+	assert.NoError(t, err)
+	assert.Equal(t, username, config.BitbucketUsername)
+	assert.Equal(t, password, config.BitbucketPassword)
+	clear()
+}
+
+func TestConfigOkIfFileDoesNotExist(t *testing.T) {
+	env()
+	config, err := LoadConfig(true)
+	assert.NoError(t, err)
+	assert.Equal(t, username, config.BitbucketUsername)
+	assert.Equal(t, password, config.BitbucketPassword)
+	clear()
+}
+
+func TestEnvCanSetConfigPath(t *testing.T) {
+	os.Setenv("BPR_CONFIG_PATH", "test/fixtures/config.env")
+	config, err := LoadConfig(true)
+	assert.NoError(t, err)
+	assert.Equal(t, username, config.BitbucketUsername)
+	assert.Equal(t, password, config.BitbucketPassword)
+	clear()
+}
--- pkg/config/main.go
+++ pkg/config/main.go
@@ -17,37 +17,66 @@ type Config struct {
 	BitbucketPassword string `mapstructure:"bitbucket_password"`
 }
 
-// LoadConfig using ~/.bpr/config.env or environment
-func LoadConfig() (config Config, err error) {
+func findLoadConfigFile(config *Config) {
+	// try from the environment first
+	envPath, envOK := os.LookupEnv("BPR_CONFIG_PATH")
+	if envOK {
+		viper.SetConfigFile(envPath)
+		err := viper.ReadInConfig()
+		if err != nil {
+			log.Println(err)
+		}
+		return
+	}
+
+	// then try the home directory
 	home, homeErr := os.UserHomeDir()
-	if homeErr == nil {
+	if homeErr != nil {
 		viper.AddConfigPath(fmt.Sprintf("%s/.bpr", home))
 		viper.SetConfigType("env")
+		err := viper.ReadInConfig()
+		if err != nil {
+			// TODO: replace with better log levels
+			log.Println(err.Error())
+		}
+		return
 	}
+}
+
+// LoadConfig using ~/.bpr/config.env or environment
+func LoadConfig(loadFile bool) (config Config, err error) {
+	viper.SetEnvPrefix("BPR")
 	viper.AutomaticEnv()
+	_ = viper.BindEnv("bitbucket_username", "BITBUCKET_USERNAME")
+	_ = viper.BindEnv("bitbucket_password", "BITBUCKET_PASSWORD", "BITBUCKET_APP_PASSWORD")
 
-	err = viper.ReadInConfig()
-
-	if err != nil {
-		log.Println(err)
+	if loadFile {
+		findLoadConfigFile(&config)
 	}
 
 	err = viper.Unmarshal(&config)
 
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
 	if utils.Empty(config.BitbucketUsername) {
 		err = errors.New("BITBUCKET_USERNAME required")
+		return
 	}
 
 	if utils.Empty(config.BitbucketPassword) {
 		err = errors.New("BITBUCKET_PASSWORD required")
+		return
 	}
 
 	return
 }
 
 // LoadConfigOrPanic does exactly what it says
-func LoadConfigOrPanic() Config {
-	config, err := LoadConfig()
+func LoadConfigOrPanic(loadFile bool) Config {
+	config, err := LoadConfig(loadFile)
 
 	if err != nil {
 		panic(err)
